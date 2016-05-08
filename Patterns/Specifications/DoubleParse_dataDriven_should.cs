5fc3a219117a98f4cd08d972b83b2eb75912e554
Pavel Egorov <xoposhiy@mail.ru>
2016-05-08 14:38:15 +0500

    Content

---
+++ Patterns/Specifications/DoubleParse_dataDriven_should.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using System.Globalization;
+
+namespace Kontur.Courses.Testing.Patterns.Specifications
+{
+	[TestFixture]
+	public class DoubleParse_should
+	{
+		[TestCase("123", ExpectedResult = 123, TestName = "integer")]
+		[TestCase("1.1", ExpectedResult = 1.1, TestName = "fraction")]
+		[TestCase("1.1e1", ExpectedResult = 1.1e1, TestName = "scientific with positive exp")]
+		[TestCase("1.1e-1", ExpectedResult = 1.1e-1, TestName = "scientific with negative exp")]
+		[TestCase("-0.1", ExpectedResult = -0.1, TestName = "negative fraction")]
+		public double withInvariantCulture_parse(string input)
+		{
+			return double.Parse(input, CultureInfo.InvariantCulture);
+		}
+	}
+}
